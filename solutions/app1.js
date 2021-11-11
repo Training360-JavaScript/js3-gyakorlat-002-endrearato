38262d6c2327f5fca8d9bed2250d224e99145ecc
endrearato <endre.arato.81@gmail.com>
2021-11-11 19:44:36 +0100

    half ready

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,8 @@
-
-
-
-
-
-
-// export default setCookie;
+function setCookie(sting) {
+    const expirationMin = 15;
+    const dateNow = new Date();
+    dateNow.setTime(dateNow.getTime() + (expirationMin * 1000 * 60));
+    const expire = dateNow.toString();
+    document.cookie = `token=${sting};expires = ${expire};`;
+};
+export default setCookie;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,27 @@
+function setCookie(name, sting) {
+    const expirationMin = 15;
+    const dateNow = new Date();
+    dateNow.setTime(dateNow.getTime() + (expirationMin * 1000 * 60));
+    const expire = dateNow.toString();
+    document.cookie = `${name}=${sting};expires = ${expire};`;
+};
+setCookie('viewed', '55');
+setCookie('uid', '354774631237');
+setCookie('ssid', 'Bx55OWbHJ0Vt_IGIF');
+
+const cookieHandler = {
+    getAll(){
+
+    },
+    toSessionStorage(name, value){
+
+    },
+    flush(){
+
+    }
+};
 
 
 
 
-
-
-// export { cookieHandler };
+export { cookieHandler };
